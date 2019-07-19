0087590916b74335e696577d4ef52af3070c6a74
timondecathlon <sonicstyle99@gmail.com>
2019-07-19 16:20:49 +0300

    upd

--- global_pass.php
+++ global_pass.php
@@ -9,17 +9,15 @@ include_once(PROJECT_ROOT.'/errors.php');
 //Формируем URL
 $isHttps = !empty($_SERVER['HTTPS']) && 'off' !== strtolower($_SERVER['HTTPS']);
 ($isHttps) ? $protocol = 'https' : $protocol = 'http';
-//если папка не в корне домена то менять )
-$hostname = $protocol.'://'.$_SERVER['HTTP_HOST'].'/';
 $parts = explode($_SERVER['HTTP_HOST'],__DIR__);
 $folder = array_pop($parts);
 define('PROJECT_URL',$protocol.'://'.$_SERVER['HTTP_HOST'].$folder);
 
 //Записываем пассы в константы
 define("DB_HOST", 'localhost');
-define("DB_NAME", 'db_name_here');
-define("DB_USER", 'db_user_here');
-define("DB_PASSWORD", 'db_pass_here');
+define("DB_NAME", 'dev');
+define("DB_USER", 'sonic');
+define("DB_PASSWORD", '20091993decSonic-tgogogo');
 
 //Для процедурки  
 $pdo = new \PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASSWORD);
